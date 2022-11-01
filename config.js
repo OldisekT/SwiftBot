f4614c2637a679792cacd7f586d7f41aeb9e5a25
OldisekT <88963084+OldisekT@users.noreply.github.com>
2022-11-01 20:05:25 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    channel: "Oldisek",
-    messageTemplate: "<:yt:1037026727391674408>@yt, **{author}** nahrál **{title}**!\n{url}",
-    channel_id: "UC9VCeGxLdiwrtx6JfnUZacQ",
-    watchInterval: 30000
-}
